cdbb841c46823d4b4e8e5941904ec9f1b99ce2f9
Owen <owen.king273@ccac.edu>
2019-03-06 17:56:50 -0500

    Small Update

--- Python2 part 1/Test1.py
+++
@@ -1,9 +0,0 @@
-main = {}
-
-y = int(input("How many siblings do you have:   "))
-
-for x in range(0,y):
-        main[str(input("What is your Sibling's Name:   "))] = {'Age': input("What is their Age:   "),'Birthday': input("When is their Birthday:   ")}
-
-print(main)
-
